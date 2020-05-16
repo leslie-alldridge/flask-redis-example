3300b26f4a60e0abd6eae626dac5cc4c8b9987bb
leslie.alldridge <leslie.alldridge@gmail.com>
2020-05-16 17:13:30 +1200

    add nltk and tokenizers

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import os
 import requests
+import operator
+import re
+import nltk
+from stop_words import stops
+from collections import Counter
+from bs4 import BeautifulSoup
+
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
@@ -7,7 +14,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 print(os.environ['APP_SETTINGS'])
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
 from models import Result
@@ -21,9 +28,34 @@ def index():
         try:
             url = request.form['url']
             r = requests.get(url)
-            print(r.text)
         except:
-            errors.append("Unable to get URL. Please ensure it's valid and try again.")
+            errors.append(
+                "Unable to get URL. Please ensure it's valid and try again.")
+            return render_template('index.html', errors=errors)
+
+        if r:
+            # process text
+            raw = BeautifulSoup(r.text, 'html.parser').get_text()
+            nltk.data.path.append('./nltk_data/')  # set path
+            tokens = nltk.word_tokenize(raw)
+            text = nltk.Text(tokens)
+            # remove punctuation, count words
+            nonPunct = re.compile('.*[A-Za-z].*')
+            raw_words = [w for w in text if nonPunct.match(w)]
+            raw_word_count = Counter(raw_words)
+            # stop words
+            no_stop_words = [w for w in raw_words if w.lower() not in stops]
+            no_stop_words_count = Counter(no_stop_words)
+            # save results
+            results = sorted(no_stop_words_count.items(),
+                             key=operator.itemgetter(1), reverse=True)
+            try:
+                result = Result(url=url, result_all=raw_word_count,
+                                result_no_stop_words=no_stop_words_count)
+                db.session.add(result)
+                db.session.commit()
+            except:
+                errors.append("Unable to add item to database.")
     return render_template('index.html', errors=errors, results=results)
 
 
