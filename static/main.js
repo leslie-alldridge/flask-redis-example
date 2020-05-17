f88bdb0f1c58c59cc783b9b414e7892a87636d38
leslie.alldridge <leslie.alldridge@gmail.com>
2020-05-18 08:53:18 +1200

    injecting timeout

--- static/main.js
+++ static/main.js
@@ -9,7 +9,7 @@
       '$log',
       '$http',
       '$timeout',
-      function ($scope, $log, $http) {
+      function ($scope, $log, $http, $timeout) {
         $scope.getResults = function () {
           $log.log('test');
 
@@ -27,29 +27,30 @@
               $log.log(error);
             });
         };
-      },
-      function getWordCount(jobID) {
-        let timeout = '';
 
-        let poller = function () {
-          // fire another request
-          $http
-            .get('/results/' + jobID)
-            .success(function (data, status, headers, config) {
-              if (status === 202) {
-                $log.log(data, status);
-              } else if (status === 200) {
-                $log.log(data);
-                $scope.wordcounts = data;
-                $timeout.cancel(timeout);
-                return false;
-              }
-              // continue to call the poller() function every 2 seconds
-              // until the timeout is cancelled
-              timeout = $timeout(poller, 2000);
-            });
-        };
-        poller();
+        function getWordCount(jobID) {
+          let timeout = '';
+
+          let poller = function () {
+            // fire another request
+            $http
+              .get('/results/' + jobID)
+              .success(function (data, status, headers, config) {
+                if (status === 202) {
+                  $log.log(data, status);
+                } else if (status === 200) {
+                  $log.log(data);
+                  $scope.wordcounts = data;
+                  $timeout.cancel(timeout);
+                  return false;
+                }
+                // continue to call the poller() function every 2 seconds
+                // until the timeout is cancelled
+                timeout = $timeout(poller, 2000);
+              });
+          };
+          poller();
+        }
       },
     ]);
 })();
