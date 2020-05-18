1b18234d134ec91f3a73639defc2d59b4cb05482
leslie.alldridge <leslie.alldridge@gmail.com>
2020-05-19 08:18:33 +1200

    UI update to include loading and errors

--- static/main.js
+++ static/main.js
@@ -10,6 +10,9 @@
       '$http',
       '$timeout',
       function ($scope, $log, $http, $timeout) {
+        $scope.submitButtonText = 'Submit';
+        $scope.loading = false;
+        $scope.urlerror = false;
         $scope.getResults = function () {
           $log.log('test');
 
@@ -22,6 +25,10 @@
             .success(function (results) {
               $log.log(results);
               getWordCount(results);
+              $scope.wordcounts = null;
+              $scope.loading = true;
+              $scope.urlerror = false;
+              $scope.submitButtonText = 'Loading...';
             })
             .error(function (error) {
               $log.log(error);
@@ -40,6 +47,8 @@
                   $log.log(data, status);
                 } else if (status === 200) {
                   $log.log(data);
+                  $scope.loading = false;
+                  $scope.submitButtonText = 'Submit';
                   $scope.wordcounts = data;
                   $timeout.cancel(timeout);
                   return false;
@@ -47,6 +56,12 @@
                 // continue to call the poller() function every 2 seconds
                 // until the timeout is cancelled
                 timeout = $timeout(poller, 2000);
+              })
+              .error(function (error) {
+                $log.log(error);
+                $scope.loading = false;
+                $scope.submitButtonText = 'Submit';
+                $scope.urlerror = true;
               });
           };
           poller();
